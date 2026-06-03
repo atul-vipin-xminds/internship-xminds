542ea62c36d28c7c189fad2923160de0382e434f
atul-vipin <me.atulvipin@gmail.com>
2026-06-03 15:57:16 +0530

    python lambda

--- lambda.py
+++ lambda.py
@@ -1,7 +1,3 @@
-employee_info = lambda *args, **kwargs: print(
-    "Name:", args[0],
-    ", Department:", kwargs["department"],
-    ", Salary:", kwargs["salary"]
-)
+employee_info = lambda *args, **kwargs: print(f"Name: {args[0]}, Department: {kwargs['department']}, Salary: {kwargs['salary']}")
 
 employee_info("Anu", department="IT", salary=65000)

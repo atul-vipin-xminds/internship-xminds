416c89107045e8e82757cbbac30eb696f4799450
atul-vipin <me.atulvipin@gmail.com>
2026-06-04 16:18:23 +0530

    iterator 1

---
+++ iterator_1.py
@@ -0,0 +1,11 @@
+employees = [
+    {"id": 1, "name": "John"},
+    {"id": 2, "name": "Sara"},
+    {"id": 3, "name": "David"}
+]
+
+emp_iter = iter(employees)
+
+print(next(emp_iter))
+print(next(emp_iter))
+print(next(emp_iter))

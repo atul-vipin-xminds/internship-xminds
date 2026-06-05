f1a319fe0f0fca1b3abcc685ff48fb8dd45075bb
atul-vipin <me.atulvipin@gmail.com>
2026-06-05 10:57:37 +0530

    python class3

--- notification.py
+++ notification.py
@@ -1,28 +1,28 @@
 class Notification:
-    def send(self, message):
-        pass
+    def __init__(self, sender):
+        self.sender = sender
 
 
 class EmailNotification(Notification):
     def send(self, message):
-        print("Email:", message)
+        print("Email from",self.sender, ":",message)
 
 
 class SMSNotification(Notification):
     def send(self, message):
-        print("SMS:", message)
+        print("SMS from",self.sender, ":",message)
 
 
 class WhatsAppNotification(Notification):
     def send(self, message):
-        print("WhatsApp:", message)
+        print("WhatsApp from",self.sender, ":",message)
 
 
 notifications = [
-    EmailNotification(),
-    SMSNotification(),
-    WhatsAppNotification()
+    EmailNotification("Admin"),
+    SMSNotification("Admin"),
+    WhatsAppNotification("Admin")
 ]
 
 for notification in notifications:
-    notification.send("Hello!")
+    notification.send("Meeting at 10 AM")

28d34eb0bb30fe3f6cf352c1538c0281c7153fc6
atul-vipin <me.atulvipin@gmail.com>
2026-06-05 10:05:06 +0530

    python class2

---
+++ notification.py
@@ -0,0 +1,28 @@
+class Notification:
+    def send(self, message):
+        pass
+
+
+class EmailNotification(Notification):
+    def send(self, message):
+        print("Email:", message)
+
+
+class SMSNotification(Notification):
+    def send(self, message):
+        print("SMS:", message)
+
+
+class WhatsAppNotification(Notification):
+    def send(self, message):
+        print("WhatsApp:", message)
+
+
+notifications = [
+    EmailNotification(),
+    SMSNotification(),
+    WhatsAppNotification()
+]
+
+for notification in notifications:
+    notification.send("Hello!")

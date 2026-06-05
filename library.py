2324698f3fb9a84c6aeeccddf5e0287881648236
atul-vipin <me.atulvipin@gmail.com>
2026-06-05 13:49:53 +0530

    python class6

--- library.py
+++ library.py
@@ -10,28 +10,39 @@ class LibraryItem:
         else:
             print("Already borrowed")
 
-    def display_info(self):  # Parent method
+    def display_info(self):      # Parent method
         print("Library Item")
 
 
-class Book(LibraryItem):  # Inheritance
+class Book(LibraryItem):         # Inheritance
     def __init__(self, title, author, book_type):
         super().__init__(title)
         self.author = author
         self.type = book_type
 
-    def display_info(self):  # Polymorphism (method overriding)
+    def display_info(self):      # Polymorphism
         status = "Available" if self._available else "Borrowed"
 
-        print(f"Title: {self._title}")
-        print(f"Author: {self.author}")
-        print(f"Type: {self.type}")
-        print(f"Status: {status}")
+        print("Title:", self._title)
+        print("Author:", self.author)
+        print("Type:", self.type)
+        print("Status:", status)
+
+
+class Member:
+    def __init__(self, name):
+        self.name = name
+
+    def borrow_book(self, book):
+        print(self.name, "is borrowing", book._title)
+        book.borrow()
 
 
 # Objects
 book1 = Book("ABC", "XYZ", "Book")
 book2 = Book("PQR", "LMN", "Magazine")
+
+member1 = Member("atul")
 
 print("Book 1:")
 book1.display_info()
@@ -40,7 +51,7 @@ print("\nBook 2:")
 book2.display_info()
 
 print("\nBorrowing Book 1...")
-book1.borrow()
+member1.borrow_book(book1)
 
 print("\nUpdated Book 1 Details:")
 book1.display_info()

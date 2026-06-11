9a33a95588001eed442789946baed0d369e54feb
atul-vipin-xminds <atul@xminds.com>
2026-06-11 13:00:56 +0530

    Update main.py

--- fastapi_learning/main.py
+++ fastapi_learning/main.py
@@ -4,19 +4,16 @@ from models import Student
 app = FastAPI()
 
 
-# GET example
 @app.get("/greet")
 def greet(name: str):
     return {"message": f"Hello {name}"}
 
 
-# POST example using query parameter
 @app.post("/greet")
 def greet_post(name: str):
     return {"message": f"Hello {name}"}
 
 
-# POST example using dictionary
 @app.post("/student")
 def create_student(student: dict):
     return {
@@ -25,13 +22,11 @@ def create_student(student: dict):
     }
 
 
-# POST example using Pydantic model
 @app.post("/students")
 def create_students(student: Student):
     return student
 
 
-# Path parameter + Pydantic model
 @app.post("/student/{course_id}")
 def create_student_with_id(course_id: int, student: Student):
     return {
